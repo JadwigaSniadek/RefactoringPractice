9cf6c15a00b0614ef1a04c2bb82925f68f105f2d
chopin111 <chopin111@o2.pl>
2019-02-03 12:44:00 +0100

    Task3 z testem

--- src/main/java/pl/sii/tasks/Task3.java
+++ src/main/java/pl/sii/tasks/Task3.java
@@ -1,4 +1,33 @@
 package pl.sii.tasks;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 public class Task3 {
+
+    private static int summerPrice = 120;
+    private static int winterPrice = 100;
+    private static double groupDiscount = 0.1;
+
+    public static double calculateTicketPrice(LocalDate date, int quantity) {
+        double price = 0;
+        if (date.getMonth().compareTo(Month.MARCH) > 0) {
+            if (date.getMonth().compareTo(Month.OCTOBER) < 0) {
+                price = (double) summerPrice;
+            }
+        }
+        if (date.getMonth().compareTo(Month.APRIL) < 0) {
+            price = (double) winterPrice;
+        }
+
+        if (date.getMonth().compareTo(Month.SEPTEMBER) > 0) {
+            price = (double) winterPrice;
+        }
+
+        if (quantity > 4) {
+            price = price - price * groupDiscount;
+        }
+
+        return price;
+    }
 }
---
+++ src/main/test/pl/sii/tasks/Task3Test.java
@@ -0,0 +1,47 @@
+package pl.sii.tasks;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class Task3Test {
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {LocalDate.of(YEAR, Month.MARCH, DAY), 1, 100},
+                {LocalDate.of(YEAR, Month.APRIL, DAY), 1, 120},
+                {LocalDate.of(YEAR, Month.MARCH, DAY), 5, 90},
+                {LocalDate.of(YEAR, Month.APRIL, DAY), 5, 108},
+                {LocalDate.of(YEAR, Month.MARCH, DAY), 4, 100},
+                {LocalDate.of(YEAR, Month.APRIL, DAY), 4, 120},
+                {LocalDate.of(YEAR, Month.OCTOBER, DAY), 5, 90},
+                {LocalDate.of(YEAR, Month.SEPTEMBER, DAY), 5, 108}
+        });
+    }
+
+    private static final int YEAR = 2018;
+    private static final int DAY = 1;
+    private static final double DELTA = 0.0;
+
+    private double correctPrice;
+    private LocalDate testDate;
+    private int quantity;
+
+    public Task3Test(LocalDate testDate, int quantity, double correctPrice) {
+        this.testDate = testDate;
+        this.quantity = quantity;
+        this.correctPrice = correctPrice;
+    }
+
+    @Test
+    public void test() {
+        Assert.assertEquals(correctPrice, Task3.calculateTicketPrice(testDate, quantity), DELTA);
+    }
+}
