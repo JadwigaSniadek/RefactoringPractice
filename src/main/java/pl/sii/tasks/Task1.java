b53a387db71f24486e06762bd738736e144f90d1
Bartol90 <lukasz.bartolik@gmail.com>
2019-03-16 13:47:22 +0100

    Refactoring metody Poprawka testu gitignore

--- src/main/java/pl/sii/tasks/Task1.java
+++ src/main/java/pl/sii/tasks/Task1.java
@@ -2,21 +2,9 @@ package pl.sii.tasks;
 
 public class Task1 {
 
-    public static boolean testSomething(int a, int b, int c) {
-        boolean result;
-        if(a<8) {
-            if(b<8) {
-                if(c<8) {
-                    result = true;
-                } else {
-                    result = false;
-                }
-            } else {
-                result = false;
-            }
-        } else {
-            result = false;
-        }
-        return result;
+    public static final int COSIEK = 8;
+
+    public static boolean areAllLessThanCosiek(int a, int b, int c) {
+        return a < COSIEK && b < COSIEK && c < COSIEK;
     }
 }
